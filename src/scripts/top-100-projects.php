2893047a08eb6be70079ab98bd9505167cc1d411
Adeel Akhter <adeelakhterit@gmail.com>
2025-02-25 05:45:40 +0400

    wip

--- src/scripts/top-100-projects.php
+++ src/scripts/top-100-projects.php
@@ -1,7 +1,61 @@
 <?php
 
+use League\Csv\Writer;
 use Symfony\Component\HttpClient\HttpClient;
 
 require_once __DIR__ . '/../init.php';
 
+$projects = [];
 
+$language = 'php';
+
+$client = HttpClient::create();
+
+$response = $client->request(
+  'GET',
+  'https://api.github.com/search/repositories',
+  [
+    'query' => [
+      'q' => sprintf('language:%s', $language),
+      'sort' => 'stars',
+      'order' => 'desc',
+      'per_page' => 100,
+    ]
+  ]
+);
+
+$data = $response->toArray();
+
+$repos = $data['items'];
+
+foreach ($repos as $repo) {
+  $projects[] = [
+    'id' => $repo['id'],
+    'name' => $repo['full_name'],
+    'stars' => $repo['watchers'],
+    'forks' => $repo['forks'],
+    'size' => $repo['size'],
+    'language' => $repo['language'],
+    'branch:default' => $repo['default_branch'],
+    'topics' => implode(', ', $repo['topics']),
+    'issues:open' => $repo['open_issues'],
+    'license' => $repo['license']['name'],
+    'url' => $repo['html_url'],
+    'owner:url' => $repo['owner']['html_url'],
+    'home' => $repo['homepage'],
+    'created_at' => $repo['created_at'],
+    'updated_at' => $repo['updated_at'],
+  ];
+}
+
+$csv = Writer::createFromPath(
+  sprintf(
+    '%s/top-100-projects-%s.csv',
+    __DIR__ . '/../../docs',
+    $language
+  ),
+  'w+'
+);
+
+$csv->insertOne(array_keys($projects));
+$csv->insertAll($projects);
