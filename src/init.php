c225a637f3a3300f951a6550db7c9c08b5bfb527
MRRazvi <mrrazvi@icloud.com>
2025-02-26 08:52:42 +0400

    wip

--- src/init.php
+++ src/init.php
@@ -1,3 +1,5 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+define('TH_REPOS_PATH', __DIR__ . '/../repos');
--- src/scripts/php-functions-count.php
+++ src/scripts/php-functions-count.php
@@ -4,8 +4,11 @@ use PhpParser\Node;
 use PhpParser\ParserFactory;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
+use Symfony\Component\Finder\Finder;
 
 require_once __DIR__ . '/../init.php';
+
+ini_set('memory_limit', '8000M');
 
 $data = [];
 
@@ -13,38 +16,58 @@ $php_functions = get_defined_functions()['internal'];
 
 class FunctionCallVisitor extends NodeVisitorAbstract
 {
+  public array $functions = [];
+
   public function enterNode(Node $node)
   {
-    global $data;
-
     if ($node instanceof Node\Expr\FuncCall) {
       if ($node->name instanceof Node\Name) {
-        if (array_key_exists($node->name->toString(), $data)) {
-          $data[$node->name->toString()]++;
-        } else {
-          $data[$node->name->toString()] = 1;
-        }
+        $this->functions[$node->name->toString()] = ($this->functions[$node->name->toString()] ?? 0) + 1;
       }
     }
+
+    return null;
   }
 }
 
+function find_php_files(string $directory): array
+{
+  $finder = new Finder();
+  $finder->files()->name('*.php')->in($directory);
+
+  $files = [];
+  foreach ($finder as $file) {
+    $files[] = $file->getRealPath();
+  }
+
+  return $files;
+}
+
+$files = find_php_files(TH_REPOS_PATH);
+
 $parser = (new ParserFactory())->createForNewestSupportedVersion();
 $traverser = new NodeTraverser;
-$visitor = new FunctionCallVisitor();
 
-try {
-  $code = file_get_contents(__DIR__ . '/test.php');
+foreach ($files as $file) {
+  try {
+    $code = file_get_contents($file);
+    $statements = $parser->parse($code);
 
-  $statements = $parser->parse($code);
-  $traverser->addVisitor($visitor);
-  $traverser->traverse($statements);
+    $visitor = new FunctionCallVisitor();
+    $traverser->addVisitor($visitor);
+    $traverser->traverse($statements);
+    $traverser->removeVisitor($visitor);
 
-  $result = array_intersect_key($data, array_flip($php_functions));
-
-  arsort($result);
-
-  dump($result);
-} catch (Exception $error) {
-  dump($error->getMessage());
+    foreach ($visitor->functions as $name => $count) {
+      $data[$name] = ($data[$name] ?? 0) + $count;
+    }
+  } catch (Exception $error) {
+    dump($error->getMessage());
+  }
 }
+
+$result = array_intersect_key($data, array_flip($php_functions));
+
+arsort($result);
+
+dump($result);
